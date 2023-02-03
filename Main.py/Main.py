e7d68da3c67da541b8df1dbdda769677144059a2
emilio barrera <barrera_emilio@yahoo.es>
2023-02-03 14:08:27 +0000

    Eliminar curos done

--- Main.py/Main.py
+++ Main.py/Main.py
@@ -4,67 +4,80 @@ import funciones
 
 
 def menuPrincipal():
-	continuar = True
+    continuar = True
 
-	while (continuar):
+    while (continuar):
 
-		opcionCorrecta = False
+        opcionCorrecta = False
 
-		while (not opcionCorrecta):
+        while (not opcionCorrecta):
 
-			print("=======  MENÚ PRINCIPAL  =======")
-			print("1.- Listar Cursos.")
-			print("2.- Registar Curso.")
-			print("3.- Actualizar Curso.")
-			print("4.- Eliminar Cursos.")
-			print("5.- Salir.")
-			print("=====  FIN MENÚ PRINCIPAL  =====")
-			print()
-			opcion = int(input("Seleccione una opción: "))
-			print()
+            print("=======  MENÚ PRINCIPAL  =======")
+            print("1.- Listar Cursos.")
+            print("2.- Registar Curso.")
+            print("3.- Actualizar Curso.")
+            print("4.- Eliminar Cursos.")
+            print("5.- Salir.")
+            print("=====  FIN MENÚ PRINCIPAL  =====")
+            print()
+            opcion = int(input("Seleccione una opción: "))
+            print()
 
-			if opcion < 1 or opcion > 5:
+            if opcion < 1 or opcion > 5:
 
-					print("Opción incorrecta, ingrese nuevamente un Opción.....")
-					print()
+                print("Opción incorrecta, ingrese nuevamente un Opción.....")
+                print()
 
-			elif opcion == 5:
+            elif opcion == 5:
 
-					continuar = False
-					print("¡Gracias por usar este Sistema!-:")
-					print()
-					break
+                continuar = False
+                print("¡Gracias por usar este Sistema!-:")
+                print()
+                break
 
-			else:
-					opcionCorrecta = True
-					ejecutarOpciones(opcion)
+            else:
+                opcionCorrecta = True
+                ejecutarOpciones(opcion)
 
 
 def ejecutarOpciones(opcion):
-  match opcion:
+    match opcion:
         case 1:
             try:
-              funciones.listarCursos()
-              print("")
+                funciones.listarCursos()
+                print("")
             except:
-              print("Listar Cursos, Ocurrió un Error!")
-              print(" ")
+                print("Listar Cursos, Ocurrió un Error!")
+                print(" ")
         case 2:
             curso = funciones.pedirDatosRegistro()
             try:
-              funciones.registrarCurso(curso)
-             
+                funciones.registrarCurso(curso)
+
             except:
-               print("Registrar Curso, Ocurrió un Error!")
-               print(" ")
+                print("Registrar Curso, Ocurrió un Error!")
+                print(" ")
         case 3:
             return "I'm a teapot"
+        case 4:
+            try:
+                cursos = funciones.listarCursoById()
+                if len(cursos) > 0:
+                    codigoEliminar = funciones.pedirDatosEliminacion(cursos)
+                    if not (codigoEliminar == ""):
+                        funciones.eliminarCurso(codigoEliminar)
+                    else:
+                        print("Código del curso no encontrado.")
+                else:
+                    print("No se encontraron Cursos.")
+            except:
+                print("Eliminar, Ocurrio un Error!")
+            print("")
         case 5:
-          return "hi"
-        case 5:
-          print("¡Gracias por usar este Sistema!-:")
-          print(" ")            
+            print("¡Gracias por usar este Sistema!-:")
+            print(" ")
         case _:
-          print("Sorry!, Opción no Valida!")	
-		
+            print("Sorry!, Opción no Valida!")
+
+
 menuPrincipal()
--- Main.py/funciones.py
+++ Main.py/funciones.py
@@ -1,66 +1,116 @@
 import mysql.connector
 from mysql.connector import Error
 from prettytable import PrettyTable
-	# x = PrettyTable()
+# x = PrettyTable()
 
 conexion = mysql.connector.connect(
-			host="localhost",
-			user="root",
-			password="root",
-			database="universidad")
-	# myCursor =  conexion.cursor()
-	# myCursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
-	# resultados = myCursor.fetchall()
-	# return resultados
-def listarCursos():
-		print("\nCursos: \n ")
-		contador = 1
-		myCursor = conexion.cursor()
-		myCursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
-		cursos = myCursor.fetchall()
-		for curso in cursos:
-			print(
-					f"{contador}.- Código: {curso[1]} | Nombre: {curso[2]} => ({curso[3]} Créditos)")
-			contador += 1
+    host="localhost",
+    user="root",
+    password="root",
+    database="universidad")
+# myCursor =  conexion.cursor()
+# myCursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
+# resultados = myCursor.fetchall()
+# return resultados
 
-		print(" ")
-		# x.field_names =['Código', 'Nombre','Créditos']
-	#   for curso in cursos:
-	#     x.add_row([curso[1], curso[2], curso[3]])
-	#   print(x)
-	# print()
+
+def listarCursoById():
+    # print("\nCursos: \n ")
+    contador = 1
+    myCursor = conexion.cursor()
+    myCursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
+    cursos = myCursor.fetchall()
+    # x = ""
+    # for curso in cursos:
+    #     x = curso[1]
+    return cursos
+
+
+listarCursoById()
+# # curso1 = listarCurso1()
+# # print(curso1)
+# print(" ")
+
+
+def listarCursos():
+    print("\nCursos: \n ")
+    contador = 1
+    myCursor = conexion.cursor()
+    myCursor.execute("SELECT * FROM curso ORDER BY nombre ASC")
+    cursos = myCursor.fetchall()
+    for curso in cursos:
+        print(
+            f"{contador}.- Código: {curso[1]} | Nombre: {curso[2]} => ({curso[3]} Créditos)")
+        contador += 1
+
+    print(" ")
+    # x.field_names =['Código', 'Nombre','Créditos']
+    #   for curso in cursos:
+    #     x.add_row([curso[1], curso[2], curso[3]])
+    #   print(x)
+    # print()
+
 
 def registrarCurso(curso):
-		try:
-				myCurso = conexion.cursor()
-				sql = "INSERT INTO curso (codigo, nombre, creditos) VALUES ('{0}', '{1}', '{2}')"
-				myCurso.execute(sql.format(curso[0], curso[1], curso[2]))
-				conexion.commit()
-				print("Curso Registrado!\n")
-		except Error as ex:
-    			print(f"Eror al intentar la Conexión: {ex}")
-       
-       
-       
+    try:
+        myCurso = conexion.cursor()
+        sql = "INSERT INTO curso (codigo, nombre, creditos) VALUES ('{0}', '{1}', '{2}')"
+        myCurso.execute(sql.format(curso[0], curso[1], curso[2]))
+        conexion.commit()
+        print("Curso Registrado!\n")
+    except Error as ex:
+        print(f"Eror al intentar la Conexión: {ex}")
+
+
 def pedirDatosRegistro():
-  codigoCorrecto = False
-  while (not codigoCorrecto):
-    codigo = input("Ingrese Código: ")
-    if len(codigo) == 5:
-      codigoCorrecto = True
-    else:
-      print("Código incorrecto: Debe tener 5 dígitios.")
-  nombre = input("Ingrese Nombre: ")
-  creditoCorrecto = False
-  while(not creditoCorrecto):
-    creditos = input("Ingrese Créditos: ")
-    if creditos.isnumeric():
-      if  (int(creditos) > 0):
-        creditoCorrecto = True
-        creditos = int(creditos)
-      else:
-        print("Los créditos deben ser mayor a 0.") 
-    else:
-      print("Créditos incorrectos: Debe ser un número únicamente y Mayor a 0.")
-  curso = (codigo, nombre, creditos)
-  return curso 
+    codigoCorrecto = False
+    while (not codigoCorrecto):
+        codigo = input("Ingrese Código: ")
+        if len(codigo) == 5:
+            codigoCorrecto = True
+        else:
+            print("Código incorrecto: Debe tener 5 dígitios.")
+    nombre = input("Ingrese Nombre: ")
+    creditoCorrecto = False
+    while (not creditoCorrecto):
+        creditos = input("Ingrese Créditos: ")
+        if creditos.isnumeric():
+            if (int(creditos) > 0):
+                creditoCorrecto = True
+                creditos = int(creditos)
+            else:
+                print("Los créditos deben ser mayor a 0.")
+        else:
+            print("Créditos incorrectos: Debe ser un número únicamente y Mayor a 0.")
+    curso = (codigo, nombre, creditos)
+    return curso
+
+
+def eliminarCurso(codigoEliminar):
+    try:
+        myCurso = conexion.cursor()
+        sql = f"Delete From curso Where codigo = '{codigoEliminar}'"
+        myCurso.execute(sql)
+        conexion.commit()
+        print(f"Curso Eliminado con Código:{codigoEliminar}!")
+        listarCursos()
+        print()
+    except Error as ex:
+        print(f"Error al intentar la coneción con BD: {ex}")
+        print()
+
+
+def pedirDatosEliminacion(codigos):
+    listarCursos()
+    existeCodigo = False
+    codigoEliminar = input("Ingrese el código del curso a Eiminar:")
+    print("")
+    for codigo in codigos:
+        if codigo[1] == codigoEliminar:
+            existeCodigo = True
+            break
+
+    if not existeCodigo:
+        codigoEliminar = " "
+
+    return codigoEliminar
